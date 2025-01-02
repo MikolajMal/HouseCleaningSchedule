2094f05e816f91762bacbaed0431bc81368cc479
Mikołaj Maliszewski <maliszewski.mik@gmail.com>
2025-01-02 08:06:42 +0100

    Cleaning task update

--- HouseCleaningSchedule/Model/CleaningTask.cs
+++ HouseCleaningSchedule/Model/CleaningTask.cs
@@ -9,10 +9,10 @@ namespace HouseCleaningSchedule.Model
 	public class CleaningTask
 	{
 		public int Id { get; set; }
-		public string? Name { get; set; }
-		public string? Description { get; set; }
+		public string Name { get; set; } = string.Empty;
+		public string Description { get; set; } = string.Empty;
 		public float EstimatedCompletionTime { get; set; }
 		public bool IsCompleted { get; set; }
-		public string? Repeatability { get; set; }
+		public int Repeatability { get; set; }
 	}
 }
