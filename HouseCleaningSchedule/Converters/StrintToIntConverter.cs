c59193610b8ae55f81df069bd7046c233481ee4e
Mikołaj Maliszewski <maliszewski.mik@gmail.com>
2025-01-02 08:06:26 +0100

    Add converter

---
+++ HouseCleaningSchedule/Converters/StrintToIntConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace HouseCleaningSchedule.Converters
+{
+	public class StrintToIntConverter : IValueConverter
+	{
+		public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			if (value is int intValue) return intValue.ToString();
+			return string.Empty;
+		}
+
+		public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			string strValue = value as string;
+			if(string.IsNullOrEmpty(strValue)) return 0;
+			if(int.TryParse(strValue, out int intValue)) return intValue;
+			return 0;
+		}
+	}
+}
