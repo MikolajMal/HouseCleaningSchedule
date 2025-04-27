6cda941f1f748d2b20e0878370943957787a5231
Mikołaj Maliszewski <maliszewski.mik@gmail.com>
2025-04-27 17:30:31 +0200

    Implementing EFCore and DbContext class

---
+++ HouseCleaningSchedule/Model/HouseDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace HouseCleaningSchedule.Model
+{
+	public class HouseDbContext : DbContext
+	{
+		public DbSet<Room> Rooms { get; set; }
+		public DbSet<CleaningTask> CleaningTasks { get; set; }
+
+		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+		{
+			optionsBuilder.UseSqlite("Data Source=HouseCleaningScheduleDatabase.db");
+		}
+	}
+}
