ed6c60c0d00c1b5d763a1f723addf2806c58ac94
Mikołaj Maliszewski <maliszewski.mik@gmail.com>
2025-05-04 14:19:12 +0200

    Add explicitly foreign key for CleaningTask

--- HouseCleaningSchedule/Model/CleaningTask.cs
+++ HouseCleaningSchedule/Model/CleaningTask.cs
@@ -12,5 +12,6 @@ namespace HouseCleaningSchedule.Model
 		public string Name { get; set; } = string.Empty;
 		public string Description { get; set; } = string.Empty;
 		public bool IsCompleted { get; set; }
+		public int RoomId { get; set; }
 	}
 }
